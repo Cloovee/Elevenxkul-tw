131d5e3c36a796986209c338ac28c76fe787757a
Cloovee <diniizhari054@gmail.com>
2026-08-04 07:31:55 +0700

    file setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<head>
+    @vite('resources/css/app.css') {{-- wajib pakai ini --}}
+</head>
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-lg text-center">
+        <h1 class="text-4xl font-bold text-green-600 mb-4">
+            🚀 Absensi Sekolah
+        </h1>
+        <p class="text-gray-600 text-lg">
+            Laravel + Blade + Tailwind v4 siap pakai!
+        </p>
+        <h1 class="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
+    </div>
+</body>
